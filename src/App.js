3e375848019e40972be3b2793c1545f371e5f4cb
Aabhusan <aabhusan@pop-os.localdomain>
2020-07-24 13:56:37 +0545

    changed in the app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with tekkon technologies
         </a>
       </header>
     </div>
